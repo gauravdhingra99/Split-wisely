0d5d7d1b1be804919a85a0559f28ca2ed5a9ec25
gaurav <gauravdhingra9999@gmail.com>
2019-03-11 02:22:18 +0530

    procfile and requiemnet

--- Billsplitter/settings.py
+++ Billsplitter/settings.py
@@ -120,11 +120,17 @@ USE_L10N = True
 USE_TZ = True
 
 
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+
+STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),
+    )
 
 LOGIN_REDIRECT_URL= 'test'
 LOGOUT_REDIRECT_URL= 'thanks'
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Billsplitter.wsgi --log-file -
